c790885e8884af660b8c199a113d78b366a7c4db
Yosef Alemneh <yosefworku67@gmail.com>
2021-07-24 23:59:26 +0300

    generate cml report

---
+++ scripts/report.py
@@ -0,0 +1,11 @@
+with open('results.txt', 'w') as result:
+      result.write('Logistic Regretion\n')
+      result.write(f'Model Accuracy : 54.21 %\n')
+
+      result.write('\n')
+      result.write('Decision Tree\n')
+      result.write(f'Model Accuracy : 57.6 %\n')
+
+      result.write('\n')
+      result.write('XGBoost\n')
+      result.write(f'Model Accuracy : 53.6 %\n')
